d1bf34602773c30d9fda2734ffaece028a42db90
JamesGonzalez <jamesanthony-gonzalezgramajo@cunc.edu.gt>
2018-08-03 01:27:00 -0600

    agregada la funcion potencia

--- src/OperacionesJames/Potencia.java
+++ src/OperacionesJames/Potencia.java
@@ -6,4 +6,56 @@ package OperacionesJames;
  */
 public class Potencia {
     
+    private double base;
+    private int potencia;
+    private double resultado;
+
+    public Potencia() {
+    }
+
+    public Potencia(double base, int potencia, double resultado) {
+        this.base = base;
+        this.potencia = potencia;
+        this.resultado = resultado;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(double resultado) {
+        this.resultado = resultado;
+    }
+
+   
+    
+   
+    
+    
+    public static Potencia operacionPotencia(int base, int potencia){
+        Potencia poten = null;
+        
+        double resultado = Math.pow(base, potencia);
+        
+        poten = new Potencia(base, potencia, resultado);
+        
+        return poten;
+    }
+    
 }
